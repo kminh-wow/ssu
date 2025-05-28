85b255892b45a82c3e17d6cffe3201533689fe53
kminh-wow <kminh9635@daum.net>
2025-05-28 22:53:20 +0900

    순서수정

--- OOP_Programming/Project_matrix_cal/src/tensor/Scalar.java
+++ OOP_Programming/Project_matrix_cal/src/tensor/Scalar.java
@@ -1,13 +1,25 @@
 package tensor;
 
 public interface Scalar {
+    void setValue(String val);//12
     String getValue();
-    void setValue(String val);
+    
+    //14
     String toString();
 
+    //15
     boolean equals(Object obj);
-    Scalar clone();
-    void add(Scalar other);
-    void multiply(Scalar other);
+    
+    //16
     int compareTo(Scalar val);
+
+    //17
+    Scalar clone();
+
+    //18
+    void add(Scalar other);
+
+    //19
+    void multiply(Scalar other);
+    
 }
--- OOP_Programming/Project_matrix_cal/src/tensor/Vector.java
+++ OOP_Programming/Project_matrix_cal/src/tensor/Vector.java
@@ -2,17 +2,33 @@ package tensor;
 
 public interface Vector {
 
-
-    Scalar getValue(int index);
-    void setValue(int index, Scalar val);
+    //11
+    void setValue(int index, Scalar val);//20
+    Scalar getValue(int index);//21
+    
+    //13
     int size();
+    
+    //14
     String toString();
+
+    //15
     boolean equals(Object obj);
+
+    //17
     Vector clone();
+
+    //20
     void add(Vector other);
+    
+    
+    //21
     void multiply(Scalar val);
+    
+    
     //30
     Matrix toVerticalMatrix();
+    
     //31
     Matrix toHorizontalMatrix();
 
